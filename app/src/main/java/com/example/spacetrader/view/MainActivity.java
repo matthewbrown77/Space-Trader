1d01a4d6518945617fcc68981dd4947729e80441
Matthew Brown <matthewbrown@gatech.edu>
2019-02-20 14:06:34 -0500

    Added main menu template.

--- app/src/main/java/com/example/spacetrader/view/MainActivity.java
+++ app/src/main/java/com/example/spacetrader/view/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.spacetrader.view;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.ArrayAdapter;
 
 import com.example.spacetrader.R;
-import com.example.spacetrader.entity.Player;
 
 public class MainActivity extends AppCompatActivity {
+
+	private TextView mTextMessage;
+
+	private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+			= new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+		@Override
+		public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+			switch (item.getItemId()) {
+				case R.id.navigation_travel:
+					mTextMessage.setText(R.string.title_travel);
+					return true;
+				case R.id.navigation_store:
+					mTextMessage.setText(R.string.title_store);
+					return true;
+				case R.id.navigation_inventory:
+					mTextMessage.setText(R.string.title_inventory);
+					return true;
+			}
+			return false;
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_create_player);
+		setContentView(R.layout.activity_main);
+
+		mTextMessage = (TextView) findViewById(R.id.message);
+		BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+		navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 	}
+
 }
